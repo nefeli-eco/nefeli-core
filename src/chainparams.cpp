05eb794c6880e464c1beba17ce9d5e6e51ed6150
Savvanis Spyros <savvaniss@yahoo.gr>
2025-01-15 00:40:49 +0200

    add genesis block as checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -148,6 +148,7 @@ public:
 
         checkpointData = {
             {
+                 {0, uint256S("0x12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2")}
             }
         };
 
@@ -242,7 +243,7 @@ public:
 
         checkpointData = {
             {
-
+                {0, uint256S("0x4966625a4b2851d9fdee139e56211a0d88575f59ed816ff5e6a63deb4e3e29a0")}
             }
         };
 
@@ -323,6 +324,7 @@ public:
 
         checkpointData = {
             {
+                {0, uint256S("530827f38f93b43ed12af0b3ad25a288dc02ed74d6d7857862df51fc56c416f9")},
             }
         };
 
